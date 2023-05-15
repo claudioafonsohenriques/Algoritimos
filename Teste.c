8129d9bbb6609c01682cdf9a0c130e89627597b2
Claúdio Henriques <124451351+claudioafonsohenriques@users.noreply.github.com>
2023-05-15 09:47:11 +0100

    Atualizar o Teste.c
    Este é um algoritmo para verificar a calculadora de 1 a 12 de um número da nossa preferência!

--- Teste.c
+++ Teste.c
@@ -3,7 +3,19 @@
 #include <locale.h>
 int main(){
 	
-	printf("Seje bem vindo(a) ao meu primeiro programa no git");
+	printf("Seje bem vindo(a) ao meu primeiro programa no git\aqui tem uma calculadora de multiplicar simples...");
+ int num;
+ int i = 0;
+
+printf("Digite um número para ver a sua tabuada: ");
+scanf("%d",&num);
+for( ;i<=12; i++){
+
+   printf("%d X %d = %d",i,num,num*i);
+}
+
+
+
 	return 0;
 
 }
